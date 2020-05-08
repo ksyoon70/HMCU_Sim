858ef914c0a8f8a553e119baa1d7653bcf6bb1e1
윤경섭 <ksyoon70@gmail.com>
2020-05-08 19:21:13 +0900

    [수정 M012]  - RecvBufferStruct 관리 추가

---
+++ RecvBufferStruct.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HMCU_Sim
+{
+    public class RecvBufferStruct
+    {
+        public byte[] buff = new byte[1024];
+        public int buffLen;
+
+        public RecvBufferStruct()
+        {
+            init();
+        }
+        public void init()
+        {
+            buff = new byte[1024];
+            buffLen = 0;
+        }
+
+        public void reset()
+        {
+            buffLen = 0;
+        }
+    }
+}
