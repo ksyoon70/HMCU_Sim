588c4f7754487cd36ba64a1a4981c46fc0d9d16f
윤경섭 <ksyoon70@gmail.com>
2020-05-28 18:02:05 +0900

    수정 [M018]  - 영상번호 값 short->ushort

--- ProcItem.cs
+++ ProcItem.cs
@@ -10,7 +10,7 @@ namespace HMCU_Sim
     {
        public UInt32 [] ProcNum;         ///처리번호
        public Byte seq;               ///전송연번
-       public short vioNum;         ///위반번호 위반촬영장치 부여
+       public ushort vioNum;         ///위반번호 위반촬영장치 부여
        public bool sndVioReq;        ///위반확인보냄
        public bool sndImgCfm;        ///영상확인보냄  
        public UInt32 ProcNumCnt;    ///보낼 처리번호 갯수 ProcNum 갯수 
